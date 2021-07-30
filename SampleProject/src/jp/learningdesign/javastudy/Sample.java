ff0e1f447633cc7be15e352a7ffe4c4dc403d7f5
reiko-ishihara <violet794.py16@gmail.com>
2021-07-31 00:14:32 +0900

    テストコミット

---
+++ SampleProject/src/jp/learningdesign/javastudy/Sample.java
@@ -0,0 +1,21 @@
+package jp.learningdesign.javastudy;
+
+public class Sample {
+/*
+ *  Sampleは、"Hello Eclipse!"と表示するプログラムです。
+ * 
+ */
+	
+/** Javadoc形式
+ * author: Reiko Ishihara
+ * Copyright: 利用条件の記載など
+ * @param args
+ */
+	public static void main(String[] args) {
+		// 次の行で、コンソールに文字力を出力します。
+		 System.out.println("Hello Eclipse！");
+	}
+
+}
+
+// github test
