4558a324388fb954f2570f57461c8677cb2acd33
reiko-ishihara <violet794.py16@gmail.com>
2021-07-31 10:32:46 +0900

    代入演算子

--- SampleProject/src/jp/learningdesign/javastudy/BmiCalc.java
+++ SampleProject/src/jp/learningdesign/javastudy/BmiCalc.java
@@ -6,13 +6,15 @@ package jp.learningdesign.javastudy;
 public class BmiCalc {
 /* BMI（体重指数）を計算するプログラム
  * BMI = 体重 / （身長 * 身長）
- * 体重 kg、　身長 m
+ * 体重 kg、　身長 cm
  */
 	public static void main(String[] args) {
 		double weight,height,bmi; 
 
 		weight = 71;
-		height = 1.8;
+		height = 180;
+		
+		height /= 100; // height = height / 100
 		bmi = weight / (height * height);
 		
 		System.out.println(bmi);
