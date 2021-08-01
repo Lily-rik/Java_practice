166749c2feca422422796f164106c31397f97d7f
reiko-ishihara <violet794.py16@gmail.com>
2021-08-01 19:40:31 +0900

    比較演算子とif文で処理を分岐する

--- SampleProject/src/jp/learningdesign/javastudy/BmiCalc.java
+++ SampleProject/src/jp/learningdesign/javastudy/BmiCalc.java
@@ -12,12 +12,21 @@ public class BmiCalc {
 		double weight,height,bmi; 
 
 		weight = 71;
-		height = 180;
-		
+		height = 180;	
 		height /= 100; // height = height / 100
+		
 		bmi = weight / (height * height);
 		
 		System.out.println(bmi);
+		
+		if (bmi < 18.5) {
+			System.out.println("やせ型です。");
+		} else if (bmi < 25) {
+			System.out.println("標準です。");
+		} else {
+			System.out.println("肥満です。");
+		}
+		
 	}
 
 }
