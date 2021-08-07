9e0d62ac438f1de86b2d3ab80077ad717a57b299
reiko-ishihara <violet794.py16@gmail.com>
2021-08-07 18:07:40 +0900

    サーブレットでプログラムを作成

--- FirstWebApp/src/main/java/jp/learningdesign/javastudy/BmiCalc.java
+++ FirstWebApp/src/main/java/jp/learningdesign/javastudy/BmiCalc.java
@@ -1,6 +1,8 @@
 package jp.learningdesign.javastudy;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,7 +26,12 @@ public class BmiCalc extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1>こんにちは、サーブレットの世界へようこそ！");
+		out.println("</body></html>");
 	}
 
 	/**
