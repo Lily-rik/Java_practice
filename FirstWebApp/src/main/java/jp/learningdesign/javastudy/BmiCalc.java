d2a55ab5b0b344b4916c885f93d4bd23623315dd
reiko-ishihara <violet794.py16@gmail.com>
2021-08-10 23:47:22 +0900

    サーブレットの連携２

--- FirstWebApp/src/main/java/jp/learningdesign/javastudy/BmiCalc.java
+++ FirstWebApp/src/main/java/jp/learningdesign/javastudy/BmiCalc.java
@@ -27,8 +27,8 @@ public class BmiCalc extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		// response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
+		response.setContentType("text/html;charset=UTF-8"); // レスポンスのheaderを送信するメソッド
+		PrintWriter out = response.getWriter(); // データを書き出す
 		out.println("<html><body>");
 		out.println("<h1>こんにちは、サーブレットの世界へようこそ！");
 		out.println("</body></html>");
@@ -39,7 +39,21 @@ public class BmiCalc extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		// doGet(request, response);
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response .getWriter();
+		String height_in ,weight_in;
+		double height, weight, bmi;
+		height_in = request.getParameter("height1");
+		weight_in = request.getParameter("weight1");
+		height = Double.parseDouble(height_in); // 実数に変換するメソッド
+		weight = Double.parseDouble(weight_in);
+		height /= 100;
+		bmi = weight /(height * height);
+		
+		out.println("<html><body>");
+		out.println("<p>あなたのBMI、体格指数は、" + bmi + "です。");
+		out.println("</body></html>");
 	}
 
 }
