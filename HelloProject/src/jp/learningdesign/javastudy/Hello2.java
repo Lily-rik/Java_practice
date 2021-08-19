f41ee43d5da384ba68ccbbe863a1118bf7d1ef50
reiko-ishihara <violet794.py16@gmail.com>
2021-08-19 22:36:23 +0900

    ファイルの追加

---
+++ HelloProject/src/jp/learningdesign/javastudy/Hello2.java
@@ -0,0 +1,10 @@
+package jp.learningdesign.javastudy;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Eclipse!");
+	}
+
+}
