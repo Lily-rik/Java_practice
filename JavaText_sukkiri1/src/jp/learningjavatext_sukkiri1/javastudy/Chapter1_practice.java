9ce1821fcb267d3b68021bac49371fcf73f31c6c
reiko-ishihara <violet794.py16@gmail.com>
2021-09-05 11:35:06 +0900

    Chapter1

---
+++ JavaText_sukkiri1/src/jp/learningjavatext_sukkiri1/javastudy/Chapter1_practice.java
@@ -0,0 +1,56 @@
+package jp.learningjavatext_sukkiri1.javastudy;
+
+class Chapter1_practice {
+
+//	1.
+//	（ア）コンパイラ
+//	（イ）インタプリタ
+//	（ウ）ソースコード
+//	（エ）バイトコード
+//	（オ）JVM (Java Virtual Machine)
+	
+	
+//	2.
+//	public static void main(String[] args) {
+//		int a, b;
+//		a = 3;
+//		b = 5;
+//		
+//		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + a * b);
+//
+//	}
+	
+//	2. 正答
+		//	掛け算の結果を変数cに入れるように指示があるため上記の記述は誤り
+//	public static void main(String[] args) {
+//		int a, b, c;
+//		a = 3;
+//		b = 5;
+//		c = a * b;  変数cにa*bの結果を代入する
+//		
+//		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+//	
+//	}
+
+	
+//	3.
+//	① boolean singi = true;
+//	② char moji = '駆';
+//	③ double pi = 3.14;
+//	④ long suji = 314159265853979L;
+//	⑤ String bunsho = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
