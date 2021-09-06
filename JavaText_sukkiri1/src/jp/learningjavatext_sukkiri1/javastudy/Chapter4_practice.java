ea4b71a23047bd58a249a760816486d9d318d37d
reiko-ishihara <violet794.py16@gmail.com>
2021-09-06 20:27:37 +0900

    Chapter4

---
+++ JavaText_sukkiri1/src/jp/learningjavatext_sukkiri1/javastudy/Chapter4_practice.java
@@ -0,0 +1,66 @@
+package jp.learningjavatext_sukkiri1.javastudy;
+
+class Chapter4_practice {
+	
+//	1.
+//		① int[] points = new int[4];
+//		② double[] weights = new double[5];
+//		③ boolean[] answers = new boolean[3];
+//		④ String[] names = new String[3];
+	
+	
+//	2.
+//	public static void main(String[] args) {
+//		int[] moneyList = {121902, 8302, 55100};
+//		
+//		for (int i = 0; i < moneyList.length; i++) {
+//			System.out.println(moneyList[i]);
+//		}
+//		System.out.println();
+//		
+//		// 拡張for文
+//		for (int lists : moneyList) {
+//			System.out.println(lists);
+//		}
+//		
+//	}
+	
+	
+//		3.
+//			5行目：NullPointerException
+//			6行目：ArrayIndexOutOfBoundsException
+	
+	
+//	4.
+//	public static void main(String[] args) {
+//		int[] numbers = {3, 4, 9};
+//		
+//		System.out.println("1桁の数字を入力してください");
+//		int input = new java.util.Scanner(System.in).nextInt();
+//		
+//		for (int i = 0; i < numbers.length; i++) {
+//			if (numbers[i] == input) {
+//				System.out.println("アタリ！");
+//				break;
+//			}
+//		}
+		
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
