1f9e570de50c25a331a7af68cd41bec0128f2010
reiko-ishihara <violet794.py16@gmail.com>
2021-09-11 03:46:46 +0900

    Chapter6

---
+++ JavaText_sukkiri1/src/jp/learningjavatext_sukkiri1/javastudy/Chapter6_practice_Main.java
@@ -0,0 +1,24 @@
+// VScode使用
+
+//import comment.Zenhan;
+//
+//public class Main {
+//  public static void main(String[] args) throws Exception {
+//    // 前半
+//    Zenhan.doWarusa();
+//    Zenhan.doTogame();
+//    // 後半
+//    comment.Kouhan.callDeae();
+//    comment.Kouhan.showMondokoro();
+//  }
+//
+//}
+
+
+// 6.4
+//   Main.class：c:¥work¥ex64¥Main.class
+//   Zenhan.class：c:¥work¥ex64¥comment¥Zenhan.class
+//   Kouhan.class：c:¥work¥ex64¥comment¥Kouhan.class
+
+// 6.5
+//   CLASSPATHはc:¥javaapp¥Koumonで設定されている
