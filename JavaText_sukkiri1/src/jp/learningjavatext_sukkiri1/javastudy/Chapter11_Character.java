ac212e6157b46be06e0a71043b2677c2a1aeb5fe
reiko-ishihara <violet794.py16@gmail.com>
2021-09-21 12:18:06 +0900

    Chapter11

---
+++ JavaText_sukkiri1/src/jp/learningjavatext_sukkiri1/javastudy/Chapter11_Character.java
@@ -0,0 +1,17 @@
+package jp.learningjavatext_sukkiri1.javastudy;
+
+public abstract class Chapter11_Character {		// 抽象クラス：抽象メソッドを含むクラスは必ずabstract付きのクラスにしなければならない
+	String name;																// もし付けずにコンパイルするとエラーになる
+	int hp;																			// この宣言をすることによって、newによるインスタンス化が禁止される
+	
+	// 逃げる
+	public void run() {
+		System.out.println(this.name + "は逃げ出した");
+	}
+	
+	// 戦う
+	public abstract void attack(Matango m);		// 抽象メソッド：現時点では何をするか確定できないメソッド
+																						// この宣言によって何もしないメソッドと区別することができる
+	
+
+}
